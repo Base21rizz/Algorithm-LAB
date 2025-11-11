7a3165e7f1f9648abe303a0bdee63aa109d4600a
Shoumik Islam <shoumikislamofficial@gmail.com>
2025-11-11 21:05:35 +0600

    Fixed the previous bug
    also made it easier by globally declarign the vectors to not call them repeatedly using pointer

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,68 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> v, p, w;
 
-double fracknap(vector<int> &v, vector<int> &p, vector<int> &w, int c)
+double fracknap(int c)
 {
     vector<pair<double, pair<int, int>>> R;
-
     for (int i = 0; i < v.size(); i++)
     {
-        double ratio = (double)p[i] / w[i];
-        R.push_back({ratio, {p[i], w[i]}});
+        double r = (double)p[i] / w[i];
+        R.push_back({r, {p[i], w[i]}});
     }
-
-    sort(R.rbegin(), R.rend());
-
-    double totalP = 0.0;
+    sort(R.begin(),R.end(), greater<pair<double,pair<int, int>>>());
+    double profit = 0.0;
     int rem_cap = c;
-
     for (int i = 0; i < R.size(); i++)
     {
         int price = R[i].second.first;
         int weight = R[i].second.second;
-
-        if (weight <= rem_cap)
+        if(weight <= rem_cap)
         {
-            totalP += price;
+            profit += price;
             rem_cap -= weight;
         }
         else
         {
-            totalP += price * ((double)rem_cap / weight);
+            profit += price * ((double)rem_cap / weight);
             break;
         }
     }
-    return totalP;
+    return profit;
 }
 
 int main()
 {
-    vector<int> v, p, w;
     int n, c;
     cin >> n >> c;
-
     for (int i = 0; i < n; i++)
     {
         int num;
         cin >> num;
         v.push_back(num);
     }
-
     for (int i = 0; i < n; i++)
     {
         int price;
         cin >> price;
         p.push_back(price);
     }
-
     for (int i = 0; i < n; i++)
     {
         int weight;
         cin >> weight;
         w.push_back(weight);
     }
-
-    cout << fixed << setprecision(2) << fracknap(v, p, w, c) << endl;
-
+    cout << fixed << setprecision(2) << fracknap(c) << endl;
     return 0;
 }
