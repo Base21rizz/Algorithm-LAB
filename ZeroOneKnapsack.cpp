4b99bbdd5be0a606f0a2a67a06b35ef9d7965ee3
Shoumik Islam <shoumikislamofficial@gmail.com>
2025-11-11 23:53:00 +0600

    Upadte code

--- ZeroOneKnapsack.cpp
+++ ZeroOneKnapsack.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int ZeroOneKnapsack(int c, vector<int> &p, vector<int> &weights, int n, vector<int> &chosen)
+vector<int> v, p, weights, chosen;
+int ksZeroOne(int n, int c)
 {
     vector<vector<int> > dp(n + 1, vector<int>(c + 1, 0));
     for (int i = 1; i <= n; i++)
     {
-        for (int w = 0; w <= c; ++w)
+        for (int w = 1; w <= c; ++w)
         {
             if (weights[i - 1] <= w)
             {
@@ -14,26 +14,24 @@ int ZeroOneKnapsack(int c, vector<int> &p, vector<int> &weights, int n, vector<i
             }
             else
             {
-                dp[i][w] = dp[i - 1][w];
+                dp[i][w] = dp[i-1][w];
             }
         }
     }
     int w = c;
     for (int i = n; i > 0; i--)
     {
-        if (dp[i][w] != dp[i - 1][w])
+        if(dp[i][w] != dp[i-1][w])
         {
             chosen.push_back(i);
-            w -= weights[i - 1];
+            w -= weights[i-1];
         }
     }
     return dp[n][c];
 }
-
 int main()
 {
     int n, c;
-    vector<int> v, p, weights;
     cin >> n >> c;
     for (int i = 0; i < n; i++)
     {
@@ -43,24 +41,19 @@ int main()
     }
     for (int i = 0; i < n; i++)
     {
-        int num;
-        cin >> num;
-        p.push_back(num);
+        int price;
+        cin >> price;
+        p.push_back(price);
     }
     for (int i = 0; i < n; i++)
     {
-
-        int num;
-        cin >> num;
-        weights.push_back(num);
+        int weight;
+        cin >> weight;
+        weights.push_back(weight);
     }
-    vector<int> chosen;
-    int MaxProfit = ZeroOneKnapsack(c, p, weights, n, chosen);
-    for (int i = chosen.size() - 1; i >= 0; i--)
-    {
-        cout << chosen[i] << ' ';
-    }
+    int maxprofit = ksZeroOne(n, c);
+    cout << maxprofit << endl << "Choosen: ";
+    for(int i = chosen.size() - 1; i>=0; i--) cout << chosen[i] << ' ';
     cout << endl;
-    cout << MaxProfit << endl;
     return 0;
 }
